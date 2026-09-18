f6ebb8a8d064b7e2e8e4d368e8ebce13fa5bd451
hatdogg33 <uncledrew697@gmail.com>
2026-09-18 01:35:18 +0000

    Fix Google Play Check Temporarily Unavailable by making the bulk play-store version response parser robust against unexpected packages (again)

--- app/src/main/java/com/moodtools/hub/networking/PlayStoreVersionClient.kt
+++ app/src/main/java/com/moodtools/hub/networking/PlayStoreVersionClient.kt
@@ -200,8 +200,6 @@ internal fun parsePlayStoreVersionResults(
                         put(packageName, parsed)
                     }
                 }
-            } else {
-                throw IllegalArgumentException("Unexpected package in batch response: $packageName")
             }
         }
     }
